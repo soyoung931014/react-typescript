2a3e8d32acae80e280e91946b2fcebada341a02b
park_so_young <soyoung931014@gmail.com>
2022-07-21 18:13:01 +0900

    chore: 라우팅추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+
+import Admin from 'pages/Admin';
+import Home from 'pages/Home';
+import User from 'pages/User';
 
 function Router() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="" element={<Home />} />
+      <Route path="user" element={<User />} />
+      <Route path="admin" element={<Admin />}>
+        <Route path="/:id" />
+      </Route>
+    </Routes>
+  );
 }
 
 export default Router;
